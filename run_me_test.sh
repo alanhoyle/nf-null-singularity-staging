3f79b122adc9d8c2045c770bb56246e6bc60d0b3
Alan Hoyle <alanh@unc.edu>
2025-05-13 13:16:39 -0400

    added docker test and made run_me_test.sh executable

--- run_me_test.sh
+++ run_me_test.sh
@@ -1,5 +1,13 @@
+#! /usr/bin/bash
+
+rm -rf output
+
 nextflow run . --output_dir output/local_run_works_with_file
 nextflow run . --output_dir output/local_run_works_with_dev_null --input /dev/null
 
+nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_file
+nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_dev_null --input /dev/null
+
 nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_works_with_file
 nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_fails_with_dev_null --input /dev/null
+
