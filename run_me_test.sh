d4a858a2a098050261e9094ea74b63fbc97a0bd4
Alan Hoyle <alanh@unc.edu>
2025-05-13 13:20:32 -0400

    print what we're doing in test script

--- run_me_test.sh
+++ run_me_test.sh
@@ -2,11 +2,17 @@
 
 rm -rf output
 
+echo running local executor tests:
+
 nextflow run . --output_dir output/local_run_works_with_file
 nextflow run . --output_dir output/local_run_works_with_dev_null --input /dev/null
 
+echo running docker executor tests:
+
 nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_file
 nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_dev_null --input /dev/null
+
+echo running apptainer executor tests:
 
 nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_works_with_file
 nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_fails_with_dev_null --input /dev/null
