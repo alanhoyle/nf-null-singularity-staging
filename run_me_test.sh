b1e52020d714bf31c981e475df0086f8540344fd
Alan Hoyle <alanh@unc.edu>
2025-05-13 14:43:11 -0400

    Only runs Docker/Apptainer tests if available

--- run_me_test.sh
+++ run_me_test.sh
@@ -7,13 +7,21 @@ echo running local executor tests:
 nextflow run . --output_dir output/local_run_works_with_file
 nextflow run . --output_dir output/local_run_works_with_dev_null --input /dev/null
 
-echo running docker executor tests:
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    echo running docker executor tests:
+    nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_file
+    nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_dev_null --input /dev/null
+else
+    echo "Skipping Docker tests: Docker is not available."
+fi
 
-nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_file
-nextflow run . -with-docker ubuntu:latest --output_dir output/docker_run_works_with_dev_null --input /dev/null
-
-echo running apptainer executor tests:
-
-nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_works_with_file
-nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_fails_with_dev_null --input /dev/null
+# Check if Apptainer is installed
+if command -v apptainer &> /dev/null; then
+    echo running apptainer executor tests:
+    nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_works_with_file
+    nextflow run . -with-apptainer ubuntu:latest --output_dir output/apptainer_run_fails_with_dev_null --input /dev/null
+else
+    echo "Skipping Apptainer tests: Apptainer is not available."
+fi
 
